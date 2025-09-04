9e9259a778aa39e7dda17cc4546ca7382340337e
Aryan Singh <134731233+Aryan2472005@users.noreply.github.com>
2025-09-05 04:44:00 +0530

    GLORIFY

--- slave1.v
+++ slave1.v
@@ -1,22 +1,9 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.09.2025 18:55:39
-// Design Name: 
-// Module Name: slave1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
+// NAME: ARYAN SINGH
+// Design Name: SLAVE1
+// Project Name: APB-PROTOCOL
 // Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
