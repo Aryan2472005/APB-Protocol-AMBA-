50195c8a5b3387f8bb9b6b39b51f3aea2a0a67ef
Aryan Singh <134731233+Aryan2472005@users.noreply.github.com>
2025-09-29 23:10:53 +0530

    Update slave2.v

--- slave2.v
+++ slave2.v
@@ -8,7 +8,7 @@
 
 
 module slave2(
-                input PCLK, PRST, PSEL, PENABLE, PWRITE,
+                input PCLK, PRST, PSEL2, PENABLE, PWRITE,
                 input [7:0] padd, pwdata,
                 output [7:0] prdata2,
                 output reg PREADY);
@@ -16,14 +16,14 @@ module slave2(
     reg [7:0] reg_add;
     reg [7:0] mem2 [63:0];
     
-    assign prdata = mem2[reg_add];
+    assign prdata2 = mem2[reg_add];
     
     always @(*) begin
         if(!PRST) PREADY = 0;
-        else if(PSEL && !PENABLE && !PWRITE) PREADY = 0;
-        else if(PSEL && !PENABLE && PWRITE) PREADY = 0;
-        else if(PSEL && PENABLE && !PWRITE) PREADY = 1;
-        else if(PSEL && PENABLE && PWRITE) PREADY = 1;
+        else if(PSEL2 && !PENABLE && !PWRITE) PREADY = 0;
+        else if(PSEL2 && !PENABLE && PWRITE) PREADY = 0;
+        else if(PSEL2 && PENABLE && !PWRITE) begin PREADY = 1; reg_add = padd; end
+        else if(PSEL2 && PENABLE && PWRITE) begin PREADY = 1; mem2[padd] = pwdata; end
         else PREADY = 0;
     end
     
